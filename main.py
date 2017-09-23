05c5caaa6645f416371b77b197c853cf71d56b44
ruslan <nikolaevra@gmail.com>
2017-09-23 14:31:09 -0400

    - innovation - checking for a saved trained classifier before trying to train from the beginning

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Data Preprocessing Template
-
 # Importing the libraries
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -7,54 +5,80 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import pickle as pk
+import os.path
 
-# Importing train datasets
-train = pd.read_csv('train.csv')
 
-# preparing training dataset
-train = train.iloc[:, [1, 2, 4, 5, 6, 7, 9]]
-train = train.dropna()
-X_train = train.iloc[:, 1:].values
-y_train = train.iloc[:, 0].values
+def file_exists():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filepath = dir_path + "/save.p"
+    return os.path.isfile(filepath)
 
-# Encoding categorical data
-labelencoder = LabelEncoder()
-X_train[:, 1] = labelencoder.fit_transform(X_train[:, 1])
 
-onehotencoder = OneHotEncoder(categorical_features=[0])
-X_train = onehotencoder.fit_transform(X_train).toarray()
-X_train = X_train[:, 1:]
+def prepare_test():
+    # Importing and formatting test dataset
+    test = pd.read_csv('test.csv')
+    test = test.iloc[:, [0, 1, 3, 4, 5, 6, 8, 11]]
 
-# Importing and formatting test dataset
-test = pd.read_csv('test.csv')
-test = test.iloc[:, [0, 1, 3, 4, 5, 6, 8, 11]]
+    test = test.dropna()
+    y = test.iloc[:, -1].values
+    x = test.iloc[:, 1:-1].values
 
-test = test.dropna()
-answers = test.iloc[:, -1].values
-X_test = test.iloc[:, 1:-1].values
+    # Encoding categorical data
+    le = LabelEncoder()
+    x[:, 1] = le.fit_transform(x[:, 1])
 
-# Encoding categorical data
-labelencoder = LabelEncoder()
-X_test[:, 1] = labelencoder.fit_transform(X_test[:, 1])
+    encoder = OneHotEncoder(categorical_features=[0])
+    x = encoder.fit_transform(x).toarray()
+    x = x[:, 1:]
 
-onehotencoder = OneHotEncoder(categorical_features=[0])
-X_test = onehotencoder.fit_transform(X_test).toarray()
-X_test = X_test[:, 1:]
+    return x, y
 
-# need to scale data before we start fitting it
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
 
-# Fitting Multiple Linear Regression to the Training set
-classifier = LogisticRegression(random_state=0)
-classifier.fit(X_train, y_train)
+def prepare_train():
+    # Importing train datasets
+    train = pd.read_csv('train.csv')
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+    # preparing training dataset
+    train = train.iloc[:, [1, 2, 4, 5, 6, 7, 9]]
+    train = train.dropna()
+    x = train.iloc[:, 1:].values
+    y = train.iloc[:, 0].values
 
-accuracy = accuracy_score(answers, y_pred)
+    # Encoding categorical data
+    le = LabelEncoder()
+    x[:, 1] = le.fit_transform(x[:, 1])
 
-pk.dump(classifier, open("save.p", "rw"))
+    encoder = OneHotEncoder(categorical_features=[0])
+    x = encoder.fit_transform(x).toarray()
+    x = x[:, 1:]
 
-# retrained = pickle.load( open( "save.p", "rb" ) )
+    return x, y
+
+
+if __name__ == "__main__":
+    # make sure to scale data before training
+    sc = StandardScaler()
+    x_train, y_train = prepare_train()
+    x_train = sc.fit_transform(x_train)
+    x_test, answers = prepare_test()
+    x_test = sc.fit_transform(x_test)
+
+    if not file_exists():
+        # Fitting Multiple Linear Regression to the Training set
+        classifier = LogisticRegression(random_state=0)
+        classifier.fit(x_train, y_train)
+        print("No save file training classifier")
+
+    else:
+        classifier = pk.load(open("save.p", "rb"))
+        print("Found save file loading classifier from there")
+
+    # Predicting the Test set results
+    y_pred = classifier.predict(x_test)
+
+    # checking accuracy of predictions
+    accuracy = accuracy_score(answers, y_pred)
+    print(accuracy)
+
+    # save classifier
+    pk.dump(classifier, open("save.p", "wb"))

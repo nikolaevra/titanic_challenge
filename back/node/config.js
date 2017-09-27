31afcf10a8567e413fa8f5bc3a489b51f9e1e860
EC2 Default User <ec2-user@ip-172-31-76-28.ec2.internal>
2017-09-27 20:27:36 +0000

    removed idea

--- back/node/config.js
+++ back/node/config.js
@@ -4,4 +4,4 @@ exports.SCRIPT = exports.PARENT + '/python/main.py';
 exports.SAVE = exports.PARENT + '/python/save.p';
 exports.GETACCURACY = [exports.SCRIPT, exports.SAVE, 'accuracy'];
 exports.CLASSIFY = [exports.SCRIPT, exports.SAVE, 'classify', JSON.stringify([1, 1, 1, 20, 1, 0, 200])];
-exports.PORT = 8765;
+exports.PORT = 8080;

6298320dc005c7f8399dc735aa7e60322a60187c
nikolaevra <nikolaevra@gmail.com>
2017-09-27 05:09:32 -0400

    - switched to express for server routing

---
+++ back/node/routes/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Ruslan on 9/27/2017.
+ */
+
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.sendFile('../views/index.html');
+});
+
+module.exports = router;
--- back/node/server.js
+++ back/node/server.js
@@ -3,9 +3,63 @@
  */
 'use strict';
 
-const https = require('https');
 const {spawn} = require('child_process');
+const path = require('path');
 const config = require('./config.js');
+const express = require('express');
+let app = express();
+
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+let index = require('./routes/index');
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'html');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+
+let USR_ACC = 0;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', function(socket){
+    USR_ACC ++;
+    console.log(`num of users ${USR_ACC}`);
+
+    socket.on('disconnect', () => {
+        USR_ACC --;
+        console.log(`num of users ${USR_ACC}`);
+    });
+
+    socket.on('classify', (msg) => {
+        pyPredictor([config.SCRIPT, config.SAVE, 'classify', msg]).then((data) => {
+            socket.emit(data);
+            console.log(`classified: ${data}`);
+        })
+    });
+
+    socket.on('accuracy', () => {
+        pyPredictor(config.GETACCURACY).then((data) => {
+            socket.emit(data);
+            console.log(`accuracy: ${data}`);
+        })
+    });
+});
+
+http.listen(config.PORT, function(){
+    console.log(`listening on http://localhost:${config.PORT}`);
+});
 
 function pyPredictor(args) {
     const script = spawn('python3', args);
@@ -23,32 +77,51 @@ function pyPredictor(args) {
     });
 }
 
-https.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("test");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-    if (req.method === 'POST') {
-        pyPredictor(config.CLASSIFY).then((data) => {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(data);
-        }).catch((err) => {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end(err);
-        });
-    } else if (req.method === 'GET') {
-        pyPredictor(config.GETACCURACY).then((data) => {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(data);
-        }).catch((err) => {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end(err);
-        });
-    } else {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Invalid API request');
-    }
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
-}).listen(config.PORT, 'localhost');
+module.exports = app;
 
-console.log(`Server running. Test by curl http://localhost:${config.PORT}/`);
+/*
+ http.createServer(function (req, res) {
+ //res.writeHead(200, {'Content-Type': 'text/plain'});
+ //res.end("test");
+
+ if (req.method === 'POST') {
+ pyPredictor(config.CLASSIFY).then((data) => {
+ res.writeHead(200, {'Content-Type': 'text/plain'});
+ res.end(data);
+ }).catch((err) => {
+ res.writeHead(500, {'Content-Type': 'text/plain'});
+ res.end(err);
+ });
+ } else if (req.method === 'GET') {
+ pyPredictor(config.GETACCURACY).then((data) => {
+ res.writeHead(200, {'Content-Type': 'text/plain'});
+ res.end(data);
+ }).catch((err) => {
+ res.writeHead(500, {'Content-Type': 'text/plain'});
+ res.end(err);
+ });
+ } else {
+ res.writeHead(500, {'Content-Type': 'text/plain'});
+ res.end('Invalid API request');
+ }
+ }).listen(config.PORT, 'localhost');
+ */
+// console.log(`Server running. Test by curl http://localhost:${config.PORT}/`);
